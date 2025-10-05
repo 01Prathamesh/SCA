376d6458dc388d44b1056b1dab3f68f1492403f4
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-10-05 17:10:10 +0530

    Create the Model Student in the backend

---
+++ sca_backend/Model/Student.cs
@@ -0,0 +1,11 @@
+// Models/Student.cs
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+    }
+}
