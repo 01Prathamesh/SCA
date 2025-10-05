718b3054b3886c627579299d93225a46f72efd76
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-10-05 17:20:36 +0530

    In Program.cs, register the StudentDAL class for dependency injection

--- sca_backend/Program.cs
+++ sca_backend/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Register StudentDAL for DI
+builder.Services.AddScoped<StudentDAL>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
